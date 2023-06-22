27892f2371d8e06d9e4e2296e85c2561ab09a369
mrussotti <matthewlax24@yahoo.com>
2023-06-22 15:29:17 -0400

    zoom in works

--- screens/TestPlay.js
+++ screens/TestPlay.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Animated } from 'react-native';
 import Map from '../components/map';
 import { map1, map2, warhammerMap } from '../maps/TestMap';
+import { PinchGestureHandler } from 'react-native-gesture-handler';
 
 const TestPlay = () => {
   // Define the initial state of the game here, such as the positions of the units
- 
+  const scale = new Animated.Value(1);
+
+  const onPinchEvent = Animated.event(
+    [{ nativeEvent: { scale: scale } }],
+    { useNativeDriver: true }
+  );
 
   return (
     <View style={styles.container}>
-      <Map mapData={warhammerMap} />  
+      <PinchGestureHandler onGestureEvent={onPinchEvent}>
+        <Animated.View style={[styles.mapy, { transform: [{ scale: scale }] }]}>
+          <Map mapData={warhammerMap} />
+        </Animated.View>
+      </PinchGestureHandler>
       {/* Add other components as needed, such as a status bar, controls, etc. */}
     </View>
   );
@@ -20,6 +30,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  mapy: {
+    flex: 1,
   },
 });
 
