b3e8b813c3118a87face664da469adfcfacd4863
mrussotti <matthewlax24@yahoo.com>
2023-06-22 14:39:25 -0400

    added map generation from files

---
+++ maps/TestMap.js
@@ -0,0 +1,13 @@
+export const map1 = [
+    ['cell1', 'cell2', 'cell3'],
+    ['cell4', 'cell5', 'cell6'],
+    ['cell7', 'cell8', 'cell9'],
+  ];
+  
+  export const map2 = [
+    ['cell10', 'cell11', 'cell12'],
+    ['cell13', 'cell14', 'cell15'],
+    ['cell16', 'cell17', 'cell18'],
+  ];
+  
+export const warhammerMap = Array(20).fill().map(() => Array(10).fill('cell'));
