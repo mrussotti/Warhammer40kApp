89fe0d44f7250e24fa403cb0e652e66cf89a6849
mrussotti <matthewlax24@yahoo.com>
2023-07-10 18:43:11 -0400

    deployed units can only move once per turn

--- components/game.js
+++ components/game.js
@@ -1,10 +1,8 @@
-//components/game.js
 import React, { useState, useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import GameMap from './gameMap';
 import { Picker } from '@react-native-picker/picker';
 import useFetchUnitsData from './FetchUnitsData';
-
 
 const Game = ({ armyId, map: initialMap }) => {
     const { isLoading, unitsData, error } = useFetchUnitsData(armyId);
@@ -12,14 +10,21 @@ const Game = ({ armyId, map: initialMap }) => {
     const [turn, setTurn] = useState(0);
     const [player, setPlayer] = useState(0);
     const [phase, setPhase] = useState(0);
-    // Ensure initialMap is a defined array, even if it's empty
     const [map, setMap] = useState(initialMap || []);
     const [unitsToDeploy, setUnitsToDeploy] = useState([]);
     const [selectedUnit, setSelectedUnit] = useState(null);
+    const [movedUnits, setMovedUnits] = useState([]);
 
     useEffect(() => {
         if (!isLoading && unitsData) {
-            setUnitsToDeploy(unitsData);
+            const unitsWithUniqueId = unitsData.map((unit, index) => ({
+                ...unit,
+                id: `${armyId}-${index}`
+            }));
+            console.log(unitsWithUniqueId)
+
+
+            setUnitsToDeploy(unitsWithUniqueId);
         }
     }, [isLoading, unitsData]);
 
@@ -40,6 +45,7 @@ const Game = ({ armyId, map: initialMap }) => {
             } else {
                 setPlayer(0);
                 setTurn(turn + 1);
+                setMovedUnits([]); // Reset the moved units list
             }
         }
     };
@@ -56,44 +62,44 @@ const Game = ({ armyId, map: initialMap }) => {
     };
 
     const handleDeploymentCellPress = (rowIndex, cellIndex) => {
-        console.log(unitsToDeploy.length) // use unitsToDeploy instead of unitsData
         if (unitsToDeploy.length > 0) {
             const newMap = [...map];
             newMap[rowIndex][cellIndex] = { player: players[player], unit: unitsToDeploy[0] };
             setMap(newMap);
             setUnitsToDeploy(unitsToDeploy.slice(1));
-            console.log(unitsToDeploy.length, "dfasdfd")
         }
     };
-    
-    
 
     const handleMovementCellPress = (rowIndex, cellIndex) => {
         if (selectedUnit) {
-            if (!map[rowIndex][cellIndex].unit) {
+            const maxMovementDistance = parseInt(selectedUnit.unit.gameData.movement);
+            const [selectedUnitRow, selectedUnitCell] = selectedUnit.position;
+            const distance = Math.abs(rowIndex - selectedUnitRow) + Math.abs(cellIndex - selectedUnitCell);
+    
+            // Check if this unit has already moved this turn
+            if (movedUnits.includes(selectedUnit.unit.id)) {
+                setSelectedUnit(null);
+                return;
+            }
+            
+            if (distance <= maxMovementDistance && !map[rowIndex][cellIndex].unit) {
                 const newMap = [...map];
                 newMap[rowIndex][cellIndex] = { player: players[player], unit: selectedUnit.unit };
-                newMap[selectedUnit.rowIndex][selectedUnit.cellIndex].unit = null;
+                newMap[selectedUnitRow][selectedUnitCell] = { player: null, unit: null };
                 setMap(newMap);
                 setSelectedUnit(null);
+                setMovedUnits([...movedUnits, selectedUnit.unit.id]);
             }
-            else {
-                if (map[rowIndex][cellIndex].player === players[player]) {
-                    setSelectedUnit({ rowIndex, cellIndex, unit: map[rowIndex][cellIndex].unit });
-                }
-            }
-        }
-        else {
+        } else {
             if (map[rowIndex][cellIndex].unit && map[rowIndex][cellIndex].player === players[player]) {
-                setSelectedUnit({ rowIndex, cellIndex, unit: map[rowIndex][cellIndex].unit });
+                setSelectedUnit({ rowIndex, cellIndex, unit: map[rowIndex][cellIndex].unit, position: [rowIndex, cellIndex] });
             }
         }
     };
-
+    
     useEffect(() => {
         switch (phases[phase]) {
             case 'Deployment':
-                console.log(unitsData + "---------------");
                 deploymentPhase(unitsData);
                 break;
             case 'Movement':
@@ -120,7 +126,7 @@ const Game = ({ armyId, map: initialMap }) => {
                 <View>
                     <Text>Select a unit to deploy:</Text>
                     <Picker selectedValue={selectedUnit} onValueChange={unit => setSelectedUnit(unit)}>
-                        {unitsData.map((unit, i) => (
+                        {unitsToDeploy.map((unit, i) => (
                             <Picker.Item key={i} label={unit.name} value={unit} />
                         ))}
                     </Picker>
@@ -130,6 +136,4 @@ const Game = ({ armyId, map: initialMap }) => {
     );
 };
 
-
 export default Game;
-
